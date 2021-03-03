b564b8131011b976f647ac9edb8073948a408632
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2021-03-03 11:49:21 +0200

    basic 1s and 10s counter with undo for last press

--- timer.ino
+++ timer.ino
@@ -1,14 +1,74 @@
-const uint8_t button4 = A3;
+const uint8_t undo_button = A3;
+const uint8_t ones_button = A2;
+const uint8_t tens_button = A1;
+const uint8_t start_button = A5;
 
-uint32_t user_input = 2520000;
+uint32_t user_input = 0;
 uint32_t start_time;
+
+uint16_t start = 0;
+uint8_t undo_state = 0;
 
 uint32_t get_min_left(uint32_t time_left);
 uint32_t get_sec_left(uint32_t time_left);
 
 void setup() {
-  pinMode(13, OUTPUT);
   Serial.begin(9600);
+
+  pinMode(undo_button, INPUT);
+  pinMode(ones_button, INPUT);
+  pinMode(tens_button, INPUT);
+  pinMode(start_button, INPUT);
+
+  while (!start) {
+
+    if (analogRead(ones_button)) {
+      while (analogRead(ones_button)) {
+      }
+      user_input += 60000;
+      undo_state = 1;
+      Serial.print("ones set: ");
+      Serial.print(get_min_left(user_input));
+      Serial.print(".");
+      Serial.println(get_sec_left(user_input));
+    }
+
+    if (analogRead(tens_button)) {
+      while (analogRead(tens_button)) {
+      }
+      user_input += 600000;
+      undo_state = 10;
+      Serial.print("tens set: ");
+      Serial.print(get_min_left(user_input));
+      Serial.print(".");
+      Serial.println(get_sec_left(user_input));
+    }
+
+    if (analogRead(undo_button)) {
+      while (analogRead(undo_button)) {
+      }
+      
+      if (undo_state == 1 && user_input > 60000) {
+        user_input -= 60000;
+      } 
+      else if (undo_state == 10 && user_input > 600000) {
+        if (user_input - 600000 >= 60000) {
+          user_input -= 600000;
+        }
+      }
+      Serial.print("undo set: ");
+      Serial.print(get_min_left(user_input));
+      Serial.print(".");
+      Serial.println(get_sec_left(user_input));
+    }
+
+    if (analogRead(start_button)) {
+      while (analogRead(start_button))  {
+      }
+      start = true;
+    }
+  }
+  
   start_time = millis();
 }
 
